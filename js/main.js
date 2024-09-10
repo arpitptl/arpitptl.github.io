342881572007cf681c88175ad73b10bb9952549a
Arpit Patel <patelarpit798@gmail.com>
2024-09-10 14:26:56 +0530

    updated projects

--- js/main.js
+++ js/main.js
@@ -880,83 +880,43 @@ $(document).ready(function () {
   
   const projectData = [
     {
-      title: "PassGen",
+      title: "AwesomeTODO",
       subTitle:
-        "You can easily generate secure and random passwords using this simple tool.",
+        "AwesomeTODO is a simple and minimalistic todo cli application built with Golang.",
       thumbnail: "../img/projects-default.jpg",
       description:
-        "PassGen is a Python CLI tool for generating secure, random passwords from the command line. It allows you to easily create strong and unique passwords for your accounts, with a variety of customization options. Simply enter a few commands and PassGen will do the rest, providing you with a secure password to use with confidence. Fast and efficient, PassGen is the perfect tool for anyone who needs to generate passwords on the fly.",
-      techStack: ["Python"],
-      srcURL: "",
+        "AwesomeTODO cli application lets you manage your todo list with ease. You can add, delete, and view your todo list with just a few commands. It is built with GO and uses In-Memory storage for storing the todo list. User can visualize the todo list in the form of colourful table.",
+      techStack: ["GO"],
+      srcURL: "https://github.com/arpitptl/go-awesome-todo-cli",
     },
     {
-      title: "CutLink",
-      subTitle: "Here you have full control over your links.",
-      thumbnail: "../img/thumbnails/cutlink-thumbnail.png",
-      description:
-        "CutLink is a modern and user-friendly web application built with React and Material UI. Our platform offers a complete solution for link shortening. With CutLink, you can create short, branded links that are easy to share. Try CutLink today and experience the power of a smarter link shortning platform!",
-      techStack: ["React", "MUI"],
-      srcURL: "",
-    },
-    {
-      title: "MindMate",
-      subTitle:
-        "Unlock the power of your mind with our AI-Driven mental health web application",
-      thumbnail: "../img/thumbnails/mindmate-thumbnail.png",
-      description:
-        "MindMate is a web application that provides mental health solutions using artificial intelligence powered by OpenAI. It is designed to help individuals struggling with mental health issues to improve their well-being and overall quality of life. The app provides personalized and confidential support to users by analyzing their symptoms, emotions, and behavior. MindMate uses advanced algorithms to identify patterns in user data and provide them with tailored recommendations for managing their mental health.",
-      techStack: ["Next.js", "React", "Node.js"],
-      srcURL: "",
-    },
-    {
-      title: "GitTrack",
-      subTitle: "Gain insights and strengthen your GitHub connections",
-      thumbnail: "../img/thumbnails/gittrack-thumbnail.png",
-      description:
-        "GitTrack - an innovative web application designed to empower GitHub users with powerful network analysis tools. With GitTrack, you can effortlessly track and analyze your followers, identifying those who are not reciprocating the follow. Take control of your GitHub presence, strengthen your connections, and optimize your network. Gain valuable insights and make informed decisions to build a strong and engaged community.",
-      techStack: ["Next.js", "TypeScript", "Tailwind CSS"],
-      srcURL: "",
-    },
-    {
-      title: "My Battery",
-      subTitle:
-        "Simple website that display the battery status of your device and the charging status.",
-      thumbnail: "../img/thumbnails/my-battery-thumbnail.png",
-      description:
-        "My Battery is a simple and minimalistic web application that displays your battery percentage, charging status, and whether the battery is low or full. It's important to note that this is a website-based application.",
-      techStack: ["JavaScript", "HTML", "Tailwind CSS", "CSS"],
-      srcURL: "",
-    },
-    {
-      title: "SysInfo",
-      subTitle: "Web based terminal emulator",
-      thumbnail: "../img/thumbnails/sys-info-thumbnail.png",
-      description:
-        "SysInfo is a web based terminal emulator which you can run basic commands like help, clear, battery, weather and etc.",
-      techStack: ["JavaScript", "HTML", "CSS", "Tailwind CSS"],
-      srcURL: "",
-    },
-    {
-      title: "Project Title",
-      subTitle:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
+      title: "Banking Management System",
+      subTitle: "A Django-based web application designed to manage different types of accounts and banking transactions.",
       thumbnail: "../img/projects-default.jpg",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi placeat magnam eveniet accusamus tenetur maxime aspernatur deleniti rerum praesentium ducimus minima facilis consectetur expedita, mollitia molestias qui dolorem quam laudantium. Repellendus sunt harum fugiat natus. dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
-      techStack: ["Stack"],
-      srcURL: "",
+        "Banking System, a Django-based web application designed to manage different types of accounts and banking transactions. This project aims to provide a scalable and flexible solution for handling various account types, deposit and withdrawal rules, and transaction history.",
+      techStack: ["Python", "Django"],
+      srcURL: "https://github.com/arpitptl/banking-system",
     },
     {
-      title: "Project Title",
+      title: "ChatApp",
       subTitle:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
+        "A chat application built with React and Node.js that allows you to chat with your friends and family.",
       thumbnail: "../img/projects-default.jpg",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi placeat magnam eveniet accusamus tenetur maxime aspernatur deleniti rerum praesentium ducimus minima facilis consectetur expedita, mollitia molestias qui dolorem quam laudantium. Repellendus sunt harum fugiat natus. dolor sit amet consectetur, adipisicing elit. Minima nobis quia et.",
-      techStack: ["Stack"],
-      srcURL: "",
+        "A chat application built with Node.js, Express, and socket.io where users can join private chat room and connect with other users. They can send/receive messages and also share their current location with other users. User can also join the private chat room and chat with other users.",
+      techStack: ["Node.js", "Express", "Socket.io"],
+      srcURL: "https://github.com/arpitptl/chat-app",
     },
-    // Add more objects for additional projectData here
+    {
+      title: "QuizApp",
+      subTitle: "A quiz application built with Python and Django that allows you to create and take quizzes.",
+      thumbnail: "../img/projects-default.jpg",
+      description:
+        "Quiz app based on Django framework, javascript, jQuery and ajax where user can register and test his/her knowledge in various topics. Admin user can manage quizzes and see the result of all the users. Admin can create quiz and also check historical results of all the users.",
+      techStack: ["Python", "Django", "JavaScript", "jQuery", "Ajax"],
+      srcURL: "https://github.com/arpitptl/quiz-app",
+    }
   ];
   
   loadContent(projectData);
